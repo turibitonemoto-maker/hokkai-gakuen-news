0ac35aedcc1cf3908b3d6e0a3f7e2d551d5a0d74
こーき <turibitonemoto@gmail.com>
2026-03-11 13:45:10 +0000

    Try fixing this error: `Runtime FirebaseError: Missing or insufficient p

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,7 @@
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
-import { initializeApp, getApps, getApp, FirebaseApp, deleteApp } from 'firebase/app';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 
